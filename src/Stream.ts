eac7208182b6c4bb1bf7cad64a12f972079abcf1
Harry Kightlinger <kightlingerh@gmail.com>
2019-10-29 09:49:36 -0700

    Initial thoughts

---
+++ src/Stream.ts
@@ -0,0 +1,778 @@
+import {isOutOfBound} from 'fp-ts/lib/Array';
+import {IO, chain as chainIO, map as mapIO} from 'fp-ts/lib/IO';
+
+declare module 'fp-ts/lib/HKT' {
+	interface URItoKind<A> {
+		Stream: Stream<A>
+	}
+}
+
+export const URI = 'Stream'
+
+export type URI = typeof URI
+
+export interface Stream<A> {
+	(): AsyncIterableIterator<A>
+}
+
+export function map<A, B>(ma: Stream<A>, f: (a: A) => B): Stream<B> {
+	return async function*() {
+		const streamA = await ma();
+		for await (const val of streamA) {
+			yield f(val);
+		}
+	}
+}
+
+export function chain<A, B>(ma: Stream<A>, f: (a: A) => Stream<B>): Stream<B> {
+	return async function*() {
+		const streamA = await ma();
+		for await (const valA of streamA) {
+			const streamB = await f(valA)();
+			for await (const valB of streamB) {
+				yield valB
+			}
+		}
+	}
+}
+
+
+export class StreamI<A> {
+	readonly log: LogEntry<A>[];
+	private subscribers: Subscriber<A>[];
+	constructor() {
+		this.log = [];
+		this.subscribers = [];
+	}
+	put(value: A): IO<void> {
+		return () => this._put(value);
+	}
+	private _put(value: A): void {
+		const logEntry: LogEntry<A> = { timestamp: Date.now(), value, offset: this.log.length }
+		this._notifySubscribers(logEntry);
+		this.log.push(logEntry);
+	}
+	private _notifySubscribers(entry: LogEntry<A>): void {
+		this.subscribers.forEach(subscriber => subscriber(entry));
+		this.subscribers = []
+	}
+	*subscribe(from: number = 0): IterableIterator<Promise<LogEntry<A>>> {
+		let logIndex = from;
+		while (true) {
+			if (isOutOfBound(logIndex, this.log)) {
+				yield new Promise(resolve => {
+					this.subscribers.push(resolve);
+				}) as Promise<LogEntry<A>>;
+			} else {
+				yield Promise.resolve(this.log[logIndex]);
+			}
+			logIndex++
+		}
+	}
+}
+
+
+
+
+
+/**
+ * Returns `true` if the option is an instance of `Some`, `false` otherwise
+ *
+ * @example
+ * import { some, none, isSome } from 'fp-ts/lib/Option'
+ *
+ * assert.strictEqual(isSome(some(1)), true)
+ * assert.strictEqual(isSome(none), false)
+ *
+ * @since 2.0.0
+ */
+export function isSome<A>(fa: Option<A>): fa is Some<A> {
+	return fa._tag === 'Some'
+}
+
+/**
+ * Returns `true` if the option is `None`, `false` otherwise
+ *
+ * @example
+ * import { some, none, isNone } from 'fp-ts/lib/Option'
+ *
+ * assert.strictEqual(isNone(some(1)), false)
+ * assert.strictEqual(isNone(none), true)
+ *
+ * @since 2.0.0
+ */
+export function isNone<A>(fa: Option<A>): fa is None {
+	return fa._tag === 'None'
+}
+
+/**
+ * We can pattern match using the `fold` function
+ *
+ * @example
+ * import { some, none, fold } from 'fp-ts/lib/Option'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ *
+ * assert.strictEqual(
+ *   pipe(
+ *     some(1),
+ *     fold(() => 'a none', a => `a some containing ${a}`)
+ *   ),
+ *   'a some containing 1'
+ * )
+ *
+ * assert.strictEqual(
+ *   pipe(
+ *     none,
+ *     fold(() => 'a none', a => `a some containing ${a}`)
+ *   ),
+ *   'a none'
+ * )
+ *
+ * @since 2.0.0
+ */
+export function fold<A, B>(onNone: () => B, onSome: (a: A) => B): (ma: Option<A>) => B {
+	return ma => (isNone(ma) ? onNone() : onSome(ma.value))
+}
+
+/**
+ * Constructs a new `Option` from a nullable type. If the value is `null` or `undefined`, returns `None`, otherwise
+ * returns the value wrapped in a `Some`
+ *
+ * @example
+ * import { none, some, fromNullable } from 'fp-ts/lib/Option'
+ *
+ * assert.deepStrictEqual(fromNullable(undefined), none)
+ * assert.deepStrictEqual(fromNullable(null), none)
+ * assert.deepStrictEqual(fromNullable(1), some(1))
+ *
+ * @since 2.0.0
+ */
+export function fromNullable<A>(a: A | null | undefined): Option<A> {
+	return a == null ? none : some(a)
+}
+
+/**
+ * Extracts the value out of the structure, if it exists. Otherwise returns `null`.
+ *
+ * @example
+ * import { some, none, toNullable } from 'fp-ts/lib/Option'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ *
+ * assert.strictEqual(
+ *   pipe(
+ *     some(1),
+ *     toNullable
+ *   ),
+ *   1
+ * )
+ * assert.strictEqual(
+ *   pipe(
+ *     none,
+ *     toNullable
+ *   ),
+ *   null
+ * )
+ *
+ * @since 2.0.0
+ */
+export function toNullable<A>(ma: Option<A>): A | null {
+	return isNone(ma) ? null : ma.value
+}
+
+/**
+ * Extracts the value out of the structure, if it exists. Otherwise returns `undefined`.
+ *
+ * @example
+ * import { some, none, toUndefined } from 'fp-ts/lib/Option'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ *
+ * assert.strictEqual(
+ *   pipe(
+ *     some(1),
+ *     toUndefined
+ *   ),
+ *   1
+ * )
+ * assert.strictEqual(
+ *   pipe(
+ *     none,
+ *     toUndefined
+ *   ),
+ *   undefined
+ * )
+ *
+ * @since 2.0.0
+ */
+export function toUndefined<A>(ma: Option<A>): A | undefined {
+	return isNone(ma) ? undefined : ma.value
+}
+
+/**
+ * Extracts the value out of the structure, if it exists. Otherwise returns the given default value
+ *
+ * @example
+ * import { some, none, getOrElse } from 'fp-ts/lib/Option'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ *
+ * assert.strictEqual(
+ *   pipe(
+ *     some(1),
+ *     getOrElse(() => 0)
+ *   ),
+ *   1
+ * )
+ * assert.strictEqual(
+ *   pipe(
+ *     none,
+ *     getOrElse(() => 0)
+ *   ),
+ *   0
+ * )
+ *
+ * @since 2.0.0
+ */
+export function getOrElse<A>(onNone: () => A): (ma: Option<A>) => A {
+	return ma => (isNone(ma) ? onNone() : ma.value)
+}
+
+/**
+ * Returns `true` if `ma` contains `a`
+ *
+ * @example
+ * import { some, none, elem } from 'fp-ts/lib/Option'
+ * import { eqNumber } from 'fp-ts/lib/Eq'
+ *
+ * assert.strictEqual(elem(eqNumber)(1, some(1)), true)
+ * assert.strictEqual(elem(eqNumber)(2, some(1)), false)
+ * assert.strictEqual(elem(eqNumber)(1, none), false)
+ *
+ * @since 2.0.0
+ */
+export function elem<A>(E: Eq<A>): (a: A, ma: Option<A>) => boolean {
+	return (a, ma) => (isNone(ma) ? false : E.equals(a, ma.value))
+}
+
+/**
+ * Returns `true` if the predicate is satisfied by the wrapped value
+ *
+ * @example
+ * import { some, none, exists } from 'fp-ts/lib/Option'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ *
+ * assert.strictEqual(
+ *   pipe(
+ *     some(1),
+ *     exists(n => n > 0)
+ *   ),
+ *   true
+ * )
+ * assert.strictEqual(
+ *   pipe(
+ *     some(1),
+ *     exists(n => n > 1)
+ *   ),
+ *   false
+ * )
+ * assert.strictEqual(
+ *   pipe(
+ *     none,
+ *     exists(n => n > 0)
+ *   ),
+ *   false
+ * )
+ *
+ * @since 2.0.0
+ */
+export function exists<A>(predicate: Predicate<A>): (ma: Option<A>) => boolean {
+	return ma => (isNone(ma) ? false : predicate(ma.value))
+}
+
+/**
+ * Returns a smart constructor based on the given predicate
+ *
+ * @example
+ * import { none, some, fromPredicate } from 'fp-ts/lib/Option'
+ *
+ * const getOption = fromPredicate((n: number) => n >= 0)
+ *
+ * assert.deepStrictEqual(getOption(-1), none)
+ * assert.deepStrictEqual(getOption(1), some(1))
+ *
+ * @since 2.0.0
+ */
+export function fromPredicate<A, B extends A>(refinement: Refinement<A, B>): (a: A) => Option<B>
+export function fromPredicate<A>(predicate: Predicate<A>): (a: A) => Option<A>
+export function fromPredicate<A>(predicate: Predicate<A>): (a: A) => Option<A> {
+	return a => (predicate(a) ? some(a) : none)
+}
+
+/**
+ * Transforms an exception into an `Option`. If `f` throws, returns `None`, otherwise returns the output wrapped in
+ * `Some`
+ *
+ * @example
+ * import { none, some, tryCatch } from 'fp-ts/lib/Option'
+ *
+ * assert.deepStrictEqual(
+ *   tryCatch(() => {
+ *     throw new Error()
+ *   }),
+ *   none
+ * )
+ * assert.deepStrictEqual(tryCatch(() => 1), some(1))
+ *
+ * @since 2.0.0
+ */
+export function tryCatch<A>(f: Lazy<A>): Option<A> {
+	try {
+		return some(f())
+	} catch (e) {
+		return none
+	}
+}
+
+/**
+ * Returns an `E` value if possible
+ *
+ * @since 2.0.0
+ */
+export function getLeft<E, A>(ma: Either<E, A>): Option<E> {
+	return ma._tag === 'Right' ? none : some(ma.left)
+}
+
+/**
+ * Returns an `A` value if possible
+ *
+ * @since 2.0.0
+ */
+export function getRight<E, A>(ma: Either<E, A>): Option<A> {
+	return ma._tag === 'Left' ? none : some(ma.right)
+}
+
+/**
+ * Returns a `Refinement` (i.e. a custom type guard) from a `Option` returning function.
+ * This function ensures that a custom type guard definition is type-safe.
+ *
+ * ```ts
+ * import { some, none, getRefinement } from 'fp-ts/lib/Option'
+ *
+ * type A = { type: 'A' }
+ * type B = { type: 'B' }
+ * type C = A | B
+ *
+ * const isA = (c: C): c is A => c.type === 'B' // <= typo but typescript doesn't complain
+ * const isA = getRefinement<C, A>(c => (c.type === 'B' ? some(c) : none)) // static error: Type '"B"' is not assignable to type '"A"'
+ * ```
+ *
+ * @since 2.0.0
+ */
+export function getRefinement<A, B extends A>(getOption: (a: A) => Option<B>): Refinement<A, B> {
+	return (a: A): a is B => isSome(getOption(a))
+}
+
+/**
+ * This is `chain` + `fromNullable`, useful when working with optional values
+ *
+ * @example
+ * import { some, none, fromNullable, mapNullable } from 'fp-ts/lib/Option'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ *
+ * interface Employee {
+ *   company?: {
+ *     address?: {
+ *       street?: {
+ *         name?: string
+ *       }
+ *     }
+ *   }
+ * }
+ *
+ * const employee1: Employee = { company: { address: { street: { name: 'high street' } } } }
+ *
+ * assert.deepStrictEqual(
+ *   pipe(
+ *     fromNullable(employee1.company),
+ *     mapNullable(company => company.address),
+ *     mapNullable(address => address.street),
+ *     mapNullable(street => street.name)
+ *   ),
+ *   some('high street')
+ * )
+ *
+ * const employee2: Employee = { company: { address: { street: {} } } }
+ *
+ * assert.deepStrictEqual(
+ *   pipe(
+ *     fromNullable(employee2.company),
+ *     mapNullable(company => company.address),
+ *     mapNullable(address => address.street),
+ *     mapNullable(street => street.name)
+ *   ),
+ *   none
+ * )
+ *
+ * @since 2.0.0
+ */
+export function mapNullable<A, B>(f: (a: A) => B | null | undefined): (ma: Option<A>) => Option<B> {
+	return ma => (isNone(ma) ? none : fromNullable(f(ma.value)))
+}
+
+/**
+ * @since 2.0.0
+ */
+export function getShow<A>(S: Show<A>): Show<Option<A>> {
+	return {
+		show: ma => (isNone(ma) ? 'none' : `some(${S.show(ma.value)})`)
+	}
+}
+
+/**
+ * @example
+ * import { none, some, getEq } from 'fp-ts/lib/Option'
+ * import { eqNumber } from 'fp-ts/lib/Eq'
+ *
+ * const E = getEq(eqNumber)
+ * assert.strictEqual(E.equals(none, none), true)
+ * assert.strictEqual(E.equals(none, some(1)), false)
+ * assert.strictEqual(E.equals(some(1), none), false)
+ * assert.strictEqual(E.equals(some(1), some(2)), false)
+ * assert.strictEqual(E.equals(some(1), some(1)), true)
+ *
+ * @since 2.0.0
+ */
+export function getEq<A>(E: Eq<A>): Eq<Option<A>> {
+	return {
+		equals: (x, y) => x === y || (isNone(x) ? isNone(y) : isNone(y) ? false : E.equals(x.value, y.value))
+	}
+}
+/**
+ * The `Ord` instance allows `Option` values to be compared with
+ * `compare`, whenever there is an `Ord` instance for
+ * the type the `Option` contains.
+ *
+ * `None` is considered to be less than any `Some` value.
+ *
+ *
+ * @example
+ * import { none, some, getOrd } from 'fp-ts/lib/Option'
+ * import { ordNumber } from 'fp-ts/lib/Ord'
+ *
+ * const O = getOrd(ordNumber)
+ * assert.strictEqual(O.compare(none, none), 0)
+ * assert.strictEqual(O.compare(none, some(1)), -1)
+ * assert.strictEqual(O.compare(some(1), none), 1)
+ * assert.strictEqual(O.compare(some(1), some(2)), -1)
+ * assert.strictEqual(O.compare(some(1), some(1)), 0)
+ *
+ * @since 2.0.0
+ */
+export function getOrd<A>(O: Ord<A>): Ord<Option<A>> {
+	return {
+		equals: getEq(O).equals,
+		compare: (x, y) => (x === y ? 0 : isSome(x) ? (isSome(y) ? O.compare(x.value, y.value) : 1) : -1)
+	}
+}
+
+/**
+ * `Apply` semigroup
+ *
+ * | x       | y       | concat(x, y)       |
+ * | ------- | ------- | ------------------ |
+ * | none    | none    | none               |
+ * | some(a) | none    | none               |
+ * | none    | some(a) | none               |
+ * | some(a) | some(b) | some(concat(a, b)) |
+ *
+ * @example
+ * import { getApplySemigroup, some, none } from 'fp-ts/lib/Option'
+ * import { semigroupSum } from 'fp-ts/lib/Semigroup'
+ *
+ * const S = getApplySemigroup(semigroupSum)
+ * assert.deepStrictEqual(S.concat(none, none), none)
+ * assert.deepStrictEqual(S.concat(some(1), none), none)
+ * assert.deepStrictEqual(S.concat(none, some(1)), none)
+ * assert.deepStrictEqual(S.concat(some(1), some(2)), some(3))
+ *
+ * @since 2.0.0
+ */
+export function getApplySemigroup<A>(S: Semigroup<A>): Semigroup<Option<A>> {
+	return {
+		concat: (x, y) => (isSome(x) && isSome(y) ? some(S.concat(x.value, y.value)) : none)
+	}
+}
+
+/**
+ * @since 2.0.0
+ */
+export function getApplyMonoid<A>(M: Monoid<A>): Monoid<Option<A>> {
+	return {
+		...getApplySemigroup(M),
+		empty: some(M.empty)
+	}
+}
+
+/**
+ * Monoid returning the left-most non-`None` value
+ *
+ * | x       | y       | concat(x, y) |
+ * | ------- | ------- | ------------ |
+ * | none    | none    | none         |
+ * | some(a) | none    | some(a)      |
+ * | none    | some(a) | some(a)      |
+ * | some(a) | some(b) | some(a)      |
+ *
+ * @example
+ * import { getFirstMonoid, some, none } from 'fp-ts/lib/Option'
+ *
+ * const M = getFirstMonoid<number>()
+ * assert.deepStrictEqual(M.concat(none, none), none)
+ * assert.deepStrictEqual(M.concat(some(1), none), some(1))
+ * assert.deepStrictEqual(M.concat(none, some(1)), some(1))
+ * assert.deepStrictEqual(M.concat(some(1), some(2)), some(1))
+ *
+ * @since 2.0.0
+ */
+export function getFirstMonoid<A = never>(): Monoid<Option<A>> {
+	return {
+		concat: (x, y) => (isNone(x) ? y : x),
+		empty: none
+	}
+}
+
+/**
+ * Monoid returning the right-most non-`None` value
+ *
+ * | x       | y       | concat(x, y) |
+ * | ------- | ------- | ------------ |
+ * | none    | none    | none         |
+ * | some(a) | none    | some(a)      |
+ * | none    | some(a) | some(a)      |
+ * | some(a) | some(b) | some(b)      |
+ *
+ * @example
+ * import { getLastMonoid, some, none } from 'fp-ts/lib/Option'
+ *
+ * const M = getLastMonoid<number>()
+ * assert.deepStrictEqual(M.concat(none, none), none)
+ * assert.deepStrictEqual(M.concat(some(1), none), some(1))
+ * assert.deepStrictEqual(M.concat(none, some(1)), some(1))
+ * assert.deepStrictEqual(M.concat(some(1), some(2)), some(2))
+ *
+ * @since 2.0.0
+ */
+export function getLastMonoid<A = never>(): Monoid<Option<A>> {
+	return {
+		concat: (x, y) => (isNone(y) ? x : y),
+		empty: none
+	}
+}
+
+/**
+ * Monoid returning the left-most non-`None` value. If both operands are `Some`s then the inner values are
+ * appended using the provided `Semigroup`
+ *
+ * | x       | y       | concat(x, y)       |
+ * | ------- | ------- | ------------------ |
+ * | none    | none    | none               |
+ * | some(a) | none    | some(a)            |
+ * | none    | some(a) | some(a)            |
+ * | some(a) | some(b) | some(concat(a, b)) |
+ *
+ * @example
+ * import { getMonoid, some, none } from 'fp-ts/lib/Option'
+ * import { semigroupSum } from 'fp-ts/lib/Semigroup'
+ *
+ * const M = getMonoid(semigroupSum)
+ * assert.deepStrictEqual(M.concat(none, none), none)
+ * assert.deepStrictEqual(M.concat(some(1), none), some(1))
+ * assert.deepStrictEqual(M.concat(none, some(1)), some(1))
+ * assert.deepStrictEqual(M.concat(some(1), some(2)), some(3))
+ *
+ * @since 2.0.0
+ */
+export function getMonoid<A>(S: Semigroup<A>): Monoid<Option<A>> {
+	return {
+		concat: (x, y) => (isNone(x) ? y : isNone(y) ? x : some(S.concat(x.value, y.value))),
+		empty: none
+	}
+}
+
+const defaultSeparate = { left: none, right: none }
+
+const identity = <A>(a: A): A => a
+
+/**
+ * @since 2.0.0
+ */
+export const option: Monad1<URI> &
+	Foldable1<URI> &
+	Traversable1<URI> &
+	Alternative1<URI> &
+	Extend1<URI> &
+	Compactable1<URI> &
+	Filterable1<URI> &
+	Witherable1<URI> &
+	MonadThrow1<URI> = {
+	URI,
+	map: (ma, f) => (isNone(ma) ? none : some(f(ma.value))),
+	of: some,
+	ap: (mab, ma) => (isNone(mab) ? none : isNone(ma) ? none : some(mab.value(ma.value))),
+	chain: (ma, f) => (isNone(ma) ? none : f(ma.value)),
+	reduce: (fa, b, f) => (isNone(fa) ? b : f(b, fa.value)),
+	foldMap: M => (fa, f) => (isNone(fa) ? M.empty : f(fa.value)),
+	reduceRight: (fa, b, f) => (isNone(fa) ? b : f(fa.value, b)),
+	traverse: <F>(F: Applicative<F>) => <A, B>(ta: Option<A>, f: (a: A) => HKT<F, B>): HKT<F, Option<B>> => {
+		return isNone(ta) ? F.of(none) : F.map(f(ta.value), some)
+	},
+	sequence: <F>(F: Applicative<F>) => <A>(ta: Option<HKT<F, A>>): HKT<F, Option<A>> => {
+		return isNone(ta) ? F.of(none) : F.map(ta.value, some)
+	},
+	zero: () => none,
+	alt: (ma, f) => (isNone(ma) ? f() : ma),
+	extend: (wa, f) => (isNone(wa) ? none : some(f(wa))),
+	compact: ma => option.chain(ma, identity),
+	separate: <A, B>(ma: Option<Either<A, B>>): Separated<Option<A>, Option<B>> => {
+		const o = option.map(ma, e => ({
+			left: getLeft(e),
+			right: getRight(e)
+		}))
+		return isNone(o) ? defaultSeparate : o.value
+	},
+	filter: <A>(fa: Option<A>, predicate: Predicate<A>): Option<A> => {
+		return isNone(fa) ? none : predicate(fa.value) ? fa : none
+	},
+	filterMap: (ma, f) => (isNone(ma) ? none : f(ma.value)),
+	partition: <A>(fa: Option<A>, predicate: Predicate<A>): Separated<Option<A>, Option<A>> => {
+		return {
+			left: option.filter(fa, a => !predicate(a)),
+			right: option.filter(fa, predicate)
+		}
+	},
+	partitionMap: (fa, f) => option.separate(option.map(fa, f)),
+	wither: <F>(F: Applicative<F>) => <A, B>(fa: Option<A>, f: (a: A) => HKT<F, Option<B>>): HKT<F, Option<B>> =>
+		isNone(fa) ? F.of(none) : f(fa.value),
+	wilt: <F>(F: Applicative<F>) => <A, B, C>(
+		fa: Option<A>,
+		f: (a: A) => HKT<F, Either<B, C>>
+	): HKT<F, Separated<Option<B>, Option<C>>> => {
+		const o = option.map(fa, a =>
+			F.map(f(a), e => ({
+				left: getLeft(e),
+				right: getRight(e)
+			}))
+		)
+		return isNone(o)
+			? F.of({
+				left: none,
+				right: none
+			})
+			: o.value
+	},
+	throwError: () => none
+}
+
+const {
+	alt,
+	ap,
+	apFirst,
+	apSecond,
+	chain,
+	chainFirst,
+	duplicate,
+	extend,
+	filter,
+	filterMap,
+	flatten,
+	foldMap,
+	map,
+	partition,
+	partitionMap,
+	reduce,
+	reduceRight,
+	compact,
+	separate,
+	fromEither
+} = pipeable(option)
+
+export {
+	/**
+	 * @since 2.0.0
+	 */
+		alt,
+	/**
+	 * @since 2.0.0
+	 */
+		ap,
+	/**
+	 * @since 2.0.0
+	 */
+		apFirst,
+	/**
+	 * @since 2.0.0
+	 */
+		apSecond,
+	/**
+	 * @since 2.0.0
+	 */
+		chain,
+	/**
+	 * @since 2.0.0
+	 */
+		chainFirst,
+	/**
+	 * @since 2.0.0
+	 */
+		duplicate,
+	/**
+	 * @since 2.0.0
+	 */
+		extend,
+	/**
+	 * @since 2.0.0
+	 */
+		filter,
+	/**
+	 * @since 2.0.0
+	 */
+		filterMap,
+	/**
+	 * @since 2.0.0
+	 */
+		flatten,
+	/**
+	 * @since 2.0.0
+	 */
+		foldMap,
+	/**
+	 * @since 2.0.0
+	 */
+		map,
+	/**
+	 * @since 2.0.0
+	 */
+		partition,
+	/**
+	 * @since 2.0.0
+	 */
+		partitionMap,
+	/**
+	 * @since 2.0.0
+	 */
+		reduce,
+	/**
+	 * @since 2.0.0
+	 */
+		reduceRight,
+	/**
+	 * @since 2.0.0
+	 */
+		compact,
+	/**
+	 * @since 2.0.0
+	 */
+		separate,
+	/**
+	 * @since 2.0.0
+	 */
+		fromEither
+}
